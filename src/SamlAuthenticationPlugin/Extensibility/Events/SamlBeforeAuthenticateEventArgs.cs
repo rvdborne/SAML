fe790716d84cdad54560198767fe2f7450b0e4af
Tony Triguero <tony.triguero@verint.com>
2020-03-03 09:20:34 -0600

    Adding INvitation manager

---
+++ src/SamlAuthenticationPlugin/Extensibility/Events/SamlBeforeAuthenticateEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using Telligent.Services.SamlAuthenticationPlugin.Components;
+using PublicEntity = Telligent.Evolution.Extensibility.Api.Entities.Version1.User;
+
+namespace Telligent.Services.SamlAuthenticationPlugin.Extensibility.Events
+{
+    public class SamlBeforeAuthenticateEventArgs
+    {
+        internal SamlBeforeAuthenticateEventArgs(PublicEntity user, SamlTokenData samlTokenData)
+        {
+            User = user;
+            SamlTokenData = samlTokenData;
+        }
+
+        public PublicEntity User { get; private set; }
+        public SamlTokenData SamlTokenData { get; private set; }
+    }
+}
---
+++ src/SamlAuthenticationPlugin/Extensibility/ISamlInvitationManager.cs
@@ -0,0 +1,11 @@
+﻿using Telligent.Evolution.Extensibility.Version1;
+using Telligent.Services.SamlAuthenticationPlugin.Components;
+
+namespace Telligent.Services.SamlAuthenticationPlugin.Extensibility
+{
+    public interface ISamlInvitationManager : ISingletonPlugin, ICategorizedPlugin
+    {
+        bool Enabled { get; }
+        void ManageInvitation(SamlTokenData samlTokenData, string invitationKey);
+    }
+}
